6fe5956cc6fc50d329b1ae296561ac26c5670bd5
benyebai <benyebai@gmail.com>
2022-05-25 17:12:41 -0700

    small change

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'amogus',
-  port: 5432,   
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
 })
 
 const getUsers = (request, response) => {
