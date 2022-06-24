867d6c4741db6b0c03df50dc1b6276d28f33fbe8
Your Name <benyebai@gmail.com>
2022-06-23 23:33:48 -0700

    added code for unit testing

--- test/test.js
+++ test/test.js
@@ -12,6 +12,22 @@ describe('api',() => {
         it("it should return if its modified", (done) => {
             chai.request(server)
                 .post("/modify")
+                .send({
+                    profile_id: "1",
+                    contact_id: "1",
+                    email: "throwtehcheasdasdsd@gmail.com",
+                    password: "password123",
+                    firstName: "Thomas",
+                    lastName: "Zheng",
+                    pronouns: "Him",
+                    phoneNum: "17787915430",
+                    country: "Canada",
+                    province: "British Columbia",
+                    city: "Surrey",
+                    postalCode: "V4N0Z3",
+                    address: "56-16233 83 ave more stuff more stuff",
+
+                  })
                 .end((err, response) => {
                     response.should.have.status(200);
                 })
